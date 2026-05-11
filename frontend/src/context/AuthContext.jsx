4c4150a84137029f522f7d2ab861b0ad2f6fcb44
ConsoleOverload <122792646+ConsoleOverload@users.noreply.github.com>
2026-05-11 15:42:54 +0530

    Revised frontend with some changes

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,57 @@
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+import { checkAuth } from "../api/authApi";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+
+  const [user, setUser] = useState(null);
+
+  const [loading, setLoading] =
+    useState(true);
+
+  const fetchUser = async () => {
+
+    try {
+
+      const res = await checkAuth();
+
+      setUser(res.data.payload);
+
+    } catch {
+
+      setUser(null);
+
+    } finally {
+
+      setLoading(false);
+
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        setUser,
+        loading,
+        fetchUser,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () =>
+  useContext(AuthContext);
