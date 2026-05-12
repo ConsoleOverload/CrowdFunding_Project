cb6e1da33f91fbda5b0626a43a964bc5d2d5ada6
shrithasriperumbuduru <shrithasriperumbuduru@gmail.com>
2026-05-12 15:19:13 +0530

    Update adminApi.js

--- frontend/src/api/adminApi.js
+++ frontend/src/api/adminApi.js
@@ -1,19 +1,21 @@
 import axiosInstance from "./axios";
 
 export const getAllCampaigns = () =>
-  axiosInstance.get("/admin/campaigns");
+  axiosInstance.get(
+    "/admin-api/campaigns"
+  );
 
 export const approveCampaign = (id) =>
   axiosInstance.put(
-    `/admin/campaigns/${id}/approve`
+    `/admin-api/campaigns/${id}/approve`
   );
 
 export const rejectCampaign = (id) =>
   axiosInstance.put(
-    `/admin/campaigns/${id}/reject`
+    `/admin-api/campaigns/${id}/reject`
   );
 
 export const deleteCampaign = (id) =>
   axiosInstance.delete(
-    `/admin/campaigns/${id}`
+    `/admin-api/campaigns/${id}`
   );
