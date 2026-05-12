74bfebe3133d7fd7a5eebb55651293db7de8988d
shrithasriperumbuduru <shrithasriperumbuduru@gmail.com>
2026-05-12 15:18:21 +0530

    Refactor import statements for better readability

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,125 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Campaigns from "./pages/Campaigns";
-import CreateCampaign from "./pages/CreateCampaign";
-import Profile from "./pages/Profile";
+import Home
+from "./pages/Home";
+
+import Login
+from "./pages/Login";
+
+import Signup
+from "./pages/Signup";
+
+import Campaigns
+from "./pages/Campaigns";
+
+import CreateCampaign
+from "./pages/CreateCampaign";
+
+import CampaignDetails
+from "./pages/CampaignDetails";
+
+import Profile
+from "./pages/Profile";
+
+import AdminDashboard
+from "./pages/AdminDashboard";
+
+import AdminProfile
+from "./pages/AdminProfile";
+
+import NotFound
+from "./pages/NotFound";
+
+import ProtectedRoutes
+from "./routes/ProtectedRoutes";
 
 function App() {
+
   return (
+
     <BrowserRouter>
 
       <Routes>
 
-        {/* HOME PAGE FIRST */}
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
+        {/* PUBLIC */}
 
-        {/* AUTH */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/"
+          element={<Home />}
+        />
 
-        {/* PAGES */}
-        <Route path="/campaigns" element={<Campaigns />} />
-        <Route path="/create" element={<CreateCampaign />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+
+        <Route
+          path="/signup"
+          element={<Signup />}
+        />
+
+        <Route
+          path="/campaigns"
+          element={<Campaigns />}
+        />
+
+        <Route
+          path="/campaigns/:id"
+          element={<CampaignDetails />}
+        />
+
+        {/* PROTECTED */}
+
+        <Route
+          path="/create"
+          element={
+            <ProtectedRoutes>
+              <CreateCampaign />
+            </ProtectedRoutes>
+          }
+        />
+
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoutes>
+              <Profile />
+            </ProtectedRoutes>
+          }
+        />
+
+        {/* ADMIN DASHBOARD */}
+
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoutes>
+              <AdminDashboard />
+            </ProtectedRoutes>
+          }
+        />
+
+        {/* ADMIN PROFILE */}
+
+        <Route
+          path="/admin/profile"
+          element={
+            <ProtectedRoutes>
+              <AdminProfile />
+            </ProtectedRoutes>
+          }
+        />
+
+        {/* 404 */}
+
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
 
       </Routes>
 
